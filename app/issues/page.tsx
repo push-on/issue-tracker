8db287dc7118811e1ba546fab0253d0ab6307035
mhimranhossain <mh.imranhossain@outlook.com>
2024-01-02 09:52:13 +0600

    style active link navbar

---
+++ app/issues/page.tsx
@@ -0,0 +1,7 @@
+export default function page () {
+	return (
+		<div>
+			<h1 className="text-3xl font-bold">Issues</h1>
+		</div>
+	)
+}
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,27 +1,37 @@
+'use client'
+
 import { AiFillBug } from "react-icons/ai"
 import Link from "next/link"
+import { usePathname } from "next/navigation"
+import classnames from "classnames"
 
 export default function Navbar() {
+	const curretPath = usePathname()
+
 	const links = [
 		{ label: "Dashboard", href: "/", },
 		{ label: "Issues", href: "/issues", }
 	]
 	return (
 		<nav className="flex space-x-6 border-b border-zinc-700 mb-5 px-5 h-14 items-center">
-			<Link href="/"><AiFillBug /></Link>
+			<Link href="/"><AiFillBug size={24} /></Link>
 			<ul className="flex space-x-6">
 				{
 					links.map((link) => (
 						<li key={link.href}>
 							<Link
 								href={link.href}
-								className="text-stone-300 hover:text-white transition-colors">
+								className={classnames({
+									"text-zinc-100": link.href === curretPath,
+									"text-zinc-400": link.href !== curretPath,
+									"hover:text-blue-400 transition-colors": true,
+								})}>
 								{link.label}
 							</Link>
 						</li>
 					))
 				}
 			</ul>
-		</nav>
+		</nav >
 	)
 }
